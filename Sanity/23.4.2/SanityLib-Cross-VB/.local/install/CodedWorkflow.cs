404971c7915ca7bf9d8a598e8e3aebdfd26c254e
LiviuPonova <liviu.ponova@uipath.com>
2023-07-20 11:46:11 +0300

    Add projects before wipe

---
+++ Sanity/23.4.2/SanityLib-Cross-VB/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace SanityLib-Cross-VB
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
